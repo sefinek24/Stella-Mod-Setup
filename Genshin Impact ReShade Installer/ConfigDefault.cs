972d8ca9a15d4a46cc9063dcf97b717fca9116a3
Sefinek <sefinek5916@gmail.com>
2023-02-01 15:52:44 +0100

    Update

--- Genshin Impact ReShade Installer/ConfigDefault.cs
+++ Genshin Impact ReShade Installer/ConfigDefault.cs
@@ -3,19 +3,14 @@ using System.Text.RegularExpressions;
 
 namespace Genshin_Impact_MP_Installer
 {
-    internal abstract class ConfigDefault // Change to Config
-    {
-        private const string Url = "https://discordapp.com/api/webhooks";
+	internal abstract class ConfigDefault // Change to Config
+	{
+		private const string Url = "https://discordapp.com/api/webhooks";
 
-        private static readonly bool Channel = !Regex.Match(Environment.UserName, "(?:sefinek)",
-            RegexOptions.IgnoreCase | RegexOptions.Singleline).Success;
+		private static readonly bool Channel = !Regex.Match(Environment.UserName, "(?:sefinek)", RegexOptions.IgnoreCase | RegexOptions.Singleline).Success;
 
-        public static readonly string MainWebHookUrl = Channel
-            ? $"{Url}/"
-            : $"{Url}/";
+		public static readonly string MainWebHookUrl = Channel ? $"{Url}/" : $"{Url}/";
 
-        public static readonly string LogsWebHookUrl = Channel
-            ? $"{Url}/"
-            : $"{Url}/";
-    }
+		public static readonly string LogsWebHookUrl = Channel ? $"{Url}/" : $"{Url}/";
+	}
 }
