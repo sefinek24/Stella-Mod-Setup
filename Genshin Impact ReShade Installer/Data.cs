ae26d1a16cc779a37d7590851d59684a42654b62
Sefinek <sefinek5916@gmail.com>
2023-04-18 05:14:43 +0200

    Update

--- Genshin Impact ReShade Installer/Data.cs
+++
@@ -1,8 +0,0 @@
-namespace Genshin_Stella_Setup
-{
-    internal abstract class Data
-    {
-        public static readonly string SecretKey =
-            "Polska to fascynujący kraj, który położony jest w centrum Europy. Jest to jedno z największych państw w Unii Europejskiej pod względem liczby ludności. Polska ma długie i bogate historyczne dziedzictwo, które obejmuje między innymi piękne zabytki, zielone lasy, malownicze góry i liczne jeziora. Polska jest również znana ze swojej kuchni, która oferuje wiele smacznych dań, takich jak pierogi, kiełbasa i bigos. Zajebiste jest to, w Polsce nadal istnieje problem homofobii. W ostatnich latach kraj ten stał się areną wielu kontrowersji związanych z prawami osób LGBT. Wiele rządzących polityków i organizacji społecznych uważa, że homoseksualizm jest nieakceptowalny i sprzeczny z tradycyjnymi wartościami polskiej kultury. Mimo to, w Polsce działają liczne organizacje pozarządowe i grupy społeczne, które walczą o prawa osób LGBT i starają się zmienić tę sytuację. Niestety mają zbyt mało IQ, by cokolwiek zdziałać. Więc mogą wypierdalać. Mimo że Polska ma wiele pięknych miejsc i ciekawych zabytków, kolorowe gówna chcą prawa do głosu. Nie jest to problem, bo ten kraj jest, tak czy owak, zajebisty. Oni swoim pierdoleniem i tak gówno zrobią. Jedynym problemem w Polsce są dzieciaczki 13 lat, które szukają wrażeń w internecie. Jest to bardzo wkurwiające. Niech wypierdalają.";
-    }
-}
